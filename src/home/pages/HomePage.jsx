ad6183327c736c45819fc5c2ce948ed0d5afea9b
waxer59 <hnboyano@gmail.com>
2024-05-25 14:10:29 +0200

    fix deploy

---
+++ src/home/pages/HomePage.jsx
@@ -0,0 +1,17 @@
+import { About } from "../components/About/About";
+import { Download } from "../components/Download/Download";
+import { Footer } from "../components/Footer/Footer";
+import { Header } from "../components/Header/Header";
+import { Navbar } from "../components/Navbar/Navbar";
+
+export const HomePage = () => {
+  return (
+    <>
+      <Navbar />
+      <Header />
+      <About />
+      <Download />
+      <Footer />
+    </>
+  );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HomePage } from "./pages/HomePage";
+import { HomePage } from "./home/pages/HomePage";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
